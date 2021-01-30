e19b91a39169ff264b6a1f17a8c70df6b94b2cdf
jkw <jkwchunjae@daum.net>
2021-01-30 19:57:59 +0900

    전경원 채팅 클라이언트

--- TestChatProgram/ChatClient_jkw/Form1.Designer.cs
+++ TestChatProgram/ChatClient_jkw/Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace ChatClient_jkw
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConnectServerButton = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ConnectServerButton
+            // 
+            this.ConnectServerButton.Location = new System.Drawing.Point(111, 51);
+            this.ConnectServerButton.Name = "ConnectServerButton";
+            this.ConnectServerButton.Size = new System.Drawing.Size(127, 41);
+            this.ConnectServerButton.TabIndex = 0;
+            this.ConnectServerButton.Text = "Connect";
+            this.ConnectServerButton.UseVisualStyleBackColor = true;
+            this.ConnectServerButton.Click += new System.EventHandler(this.ConnectServerButton_Click);
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(111, 107);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(267, 23);
+            this.MessageTextBox.TabIndex = 1;
+            this.MessageTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MessageTextBox_KeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(419, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(369, 317);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.ConnectServerButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConnectServerButton;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- TestChatProgram/ChatClient_jkw/Form1.cs
+++ TestChatProgram/ChatClient_jkw/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +14,90 @@ namespace ChatClient_jkw
 {
     public partial class Form1 : Form
     {
+        Socket connection = null;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private async void ConnectServerButton_Click(object sender, EventArgs e)
+        {
+            if (connection?.Connected ?? false)
+            {
+                var result = MessageBox.Show("Already connected. Reconnect?", "", MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+                connection.Disconnect(false);
+                connection.Close();
+                connection = null;
+            }
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("127.0.0.1");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.  
+            try
+            {
+                Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                await client.ConnectAsync(remoteEP);
+
+                richTextBox1.Text += $"Connected: {client.Connected} \n";
+
+                connection = client;
+
+                await HandleReceiveAsync();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Error: " + ex.Message;
+                connection?.Close();
+                connection = null;
+            }
+       }
+
+        private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var connected = (connection?.Connected ?? false);
+                if (!connected)
+                {
+                    MessageBox.Show("연결 먼저 하세요");
+                    return;
+                }
+
+                Task.Run(async () =>
+                {
+                    var buffer = Encoding.UTF8.GetBytes(MessageTextBox.Text);
+                    await connection.SendAsync(buffer, SocketFlags.None);
+                }).Wait();
+            }
+        }
+
+        private async Task HandleReceiveAsync()
+        {
+            var buffer = new byte[10];
+            while (true)
+            {
+                var receiveCount = await connection.ReceiveAsync(buffer, SocketFlags.None);
+                if (receiveCount == 0)
+                {
+                    if (connection.Connected)
+                    {
+                        richTextBox1.Text += "receive 0 but connected. \n";
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "receive 0 and disconnected. \n";
+                    }
+                    break;
+                }
+                var receiveData = Encoding.UTF8.GetString(buffer, 0, receiveCount);
+                richTextBox1.Text += receiveData + "\n";
+            }
+        }
     }
 }
