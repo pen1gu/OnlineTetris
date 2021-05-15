18b429596f06bb72833abf3feb82811c16166948
윤현준 <kkkths@naver.com>
2021-05-15 19:48:53 +0900

    create
    선생님 코드 따라 해보고 이해하는 중

--- TestChatProgram/ChatClient_yhj/Form1.Designer.cs
+++ TestChatProgram/ChatClient_yhj/Form1.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace ChatClient_yhj
+﻿
+namespace ChatClient_jkw
 {
-    partial class Form1
+    partial class MainClientForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,13 +29,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConnectLocalServerButton = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ConnectRemoteServerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConnectLocalServerButton
+            // 
+            this.ConnectLocalServerButton.Location = new System.Drawing.Point(12, 12);
+            this.ConnectLocalServerButton.Name = "ConnectLocalServerButton";
+            this.ConnectLocalServerButton.Size = new System.Drawing.Size(127, 41);
+            this.ConnectLocalServerButton.TabIndex = 0;
+            this.ConnectLocalServerButton.Text = "Connect Local";
+            this.ConnectLocalServerButton.UseVisualStyleBackColor = true;
+            this.ConnectLocalServerButton.Click += new System.EventHandler(this.ConnectServerButton_Click);
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(12, 68);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(267, 23);
+            this.MessageTextBox.TabIndex = 1;
+            this.MessageTextBox.TextChanged += new System.EventHandler(this.MessageTextBox_TextChanged);
+            this.MessageTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MessageTextBox_KeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(316, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(369, 317);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // ConnectRemoteServerButton
+            // 
+            this.ConnectRemoteServerButton.Location = new System.Drawing.Point(152, 12);
+            this.ConnectRemoteServerButton.Name = "ConnectRemoteServerButton";
+            this.ConnectRemoteServerButton.Size = new System.Drawing.Size(127, 41);
+            this.ConnectRemoteServerButton.TabIndex = 3;
+            this.ConnectRemoteServerButton.Text = "Connect Server";
+            this.ConnectRemoteServerButton.UseVisualStyleBackColor = true;
+            this.ConnectRemoteServerButton.Click += new System.EventHandler(this.ConnectRemoteServerButton_Click);
+            // 
+            // MainClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.ConnectRemoteServerButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.ConnectLocalServerButton);
+            this.Name = "MainClientForm";
+            this.Text = "MainClientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConnectLocalServerButton;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button ConnectRemoteServerButton;
     }
 }
 
--- TestChatProgram/ChatClient_yhj/Form1.cs
+++ TestChatProgram/ChatClient_yhj/Form1.cs
@@ -1,21 +1,151 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ChatClient_yhj
+namespace ChatClient_jkw
 {
-    public partial class Form1 : Form
+    public partial class MainClientForm : Form
     {
-        public Form1()
+        SocketEx connection = null;
+        public MainClientForm()
         {
             InitializeComponent();
         }
 
+        private void ConnectServerButton_Click(object sender, EventArgs e)
+        {
+            Connect(IPAddress.Loopback); // ip 주소 연결
+        }
+
+        private async void Connect(IPAddress ip) // 이 작업 끝나기 전까지는 다른 작업 안함.
+        {
+            if (connection?.Connected ?? false)
+            {
+                var result = MessageBox.Show("Already connected. Reconnect?", "", MessageBoxButtons.YesNo);// 재 연결 하시겠습니까?
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+
+                connection.Disconnect(false);
+                connection.Close();
+                connection = null;
+            }
+            //IPAddress ipAddress = IPAddress.Parse("221.143.21.37");
+            IPEndPoint remoteEP = new IPEndPoint(ip, 52217);
+
+            // Create a TCP/IP socket.  
+            try
+            {
+                Socket server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                await server.ConnectAsync(remoteEP);
+
+                //richTextBox1.Text += $"Connected: {server.Connected} \n";
+
+                connection = new SocketEx(server);
+
+                await connection.SendMessageAsync(new CS_Login
+                {
+                    UserName = "경원",
+                });
+
+                await HandleReceiveAsync();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Error: " + ex.Message;
+                connection?.Close();
+                connection = null;
+            }
+        }
+
+        private async void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var connected = connection?.Connected ?? false;
+                if (!connected)
+                {
+                    MessageBox.Show("연결 먼저 하세요");
+                    return;
+                }
+
+                await connection
+                    .SendMessageAsync(new CS_Message
+                    {
+                        Text = MessageTextBox.Text,
+                    });
+            }
+        }
+
+        private async Task HandleReceiveAsync() // 이 부분 질문. -> 
+        {
+            while (true)
+            {
+                var (receiveCount, receiveText) = await connection.ReceiveMessageAsync();
+                if (receiveCount == 0)
+                {
+                    if (connection.Connected)
+                    {
+                        richTextBox1.Text += "receive 0 but connected. \n";
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "receive 0 and disconnected. \n";
+                    }
+                    break;
+                }
+
+                richTextBox1.Text += receiveText + "\n";
+
+                var obj = JObject.Parse(receiveText);
+                if (!obj.ContainsKey("Type"))
+                {
+                    continue;
+                }
+
+                HandlePacketAsync(obj);
+            }
+        }
+
+        private void HandlePacketAsync(JObject packetObj)
+        {
+            var packetType = Enum.Parse<PacketType>(packetObj.Value<string>("Type"));
+
+            if (packetType == PacketType.SC_LoginAllow)
+            {
+                var packet = packetObj.ToObject<SC_LoginAllow>();
+                //Handle_SC_LoginAllow(packet);
+            }
+            else if (packetType == PacketType.SC_Message)
+            {
+                var packet = packetObj.ToObject<SC_Message>();
+
+                richTextBox1.Text += $"[{packet.UserName}] {packet.Message}\n";
+            }
+        }
+
+
+        private void ConnectRemoteServerButton_Click(object sender, EventArgs e)
+        {
+            IPAddress ipAddress = IPAddress.Parse("221.143.21.37");
+            Connect(ipAddress);
+        }
+
+        private void MessageTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
