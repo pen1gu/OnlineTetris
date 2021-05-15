9e851ee9bad94693e946457d0f5b41ad4512f627
윤현준 <kkkths@naver.com>
2021-05-15 19:49:29 +0900

    update
    오류 : 지정해준 클래스 명을 인식하지 못함

--- TestChatProgram/ChatClient_yhj/Program.cs
+++ TestChatProgram/ChatClient_yhj/Program.cs
@@ -17,7 +17,7 @@ namespace ChatClient_yhj
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainClientForm());
         }
     }
 }
