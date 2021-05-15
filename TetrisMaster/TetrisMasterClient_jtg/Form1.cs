17e1779230e755ced533989e5df072f8f26bedda
matt4728 <50314810+matt4728@users.noreply.github.com>
2021-05-15 20:05:50 +0900

    Tetris 기본 동작 구현
    서버 연결, 로그인 구현

---
+++ TetrisMaster/TetrisMasterClient_jtg/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Common;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TetrisMasterClient_jtg
+{
+    public partial class Form1 : Form
+    {
+        SocketEx connection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connect(IPAddress.Loopback);
+        }
+
+        private async void Connect(IPAddress ip)
+        {
+            if (connection?.Connected ?? false)
+            {
+                var result = MessageBox.Show("이미 서버에 연결되어 있습니다.");
+                return;
+            }
+
+            IPEndPoint remoteEP = new IPEndPoint(ip, 52217);
+ 
+            try
+            {
+                Socket server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                await server.ConnectAsync(remoteEP);
+
+                connection = new SocketEx(server);
+
+                await connection.SendMessageAsync(new CS_Login
+                {
+                    UserName = "jtg",
+                });
+
+                await HandleReceiveAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"서버 연결에 실패하였습니다. {ex.Message}");
+                connection?.Close();
+                connection = null;
+            }
+        }
+
+        private async Task HandleReceiveAsync()
+        {
+            while (true)
+            {
+                var (receiveCount, receiveText) = await connection.ReceiveMessageAsync();
+                if (receiveCount == 0)
+                {
+                    if (connection.Connected)
+                    {
+                        
+                    }
+                    else
+                    {
+                        
+                    }
+                    break;
+                }
+
+                
+
+                var obj = JObject.Parse(receiveText);
+                if (!obj.ContainsKey("Type"))
+                {
+                    continue;
+                }
+
+                HandlePacketAsync(obj);
+            }
+        }
+
+        private void HandlePacketAsync(JObject packetObj)
+        {
+            var packetType = Enum.Parse<PacketType>(packetObj.Value<string>("Type"));
+
+            if (packetType == PacketType.SC_LoginAllow)
+            {
+                var packet = packetObj.ToObject<SC_LoginAllow>();
+                Handle_SC_LoginAllow(packet);
+            }
+            // do something
+//          else if (packetType == PacketType.SC_Message)
+//          {
+//                
+//
+//               
+//          }
+        }
+
+        private void Handle_SC_LoginAllow(SC_LoginAllow packet)
+        {
+            if (packet.Allow)
+            {
+                MessageBox.Show($"서버 연결에 성공하였습니다!");
+            }
+            else
+            {
+                MessageBox.Show($"서버 연결에 실패하였습니다. {packet.DeniedReason}");
+            }
+        }
+
+        private async void StartBtn_Click(object sender, EventArgs e)
+        {
+            var connected = connection?.Connected ?? false;
+
+            if (!connected)
+            {
+                MessageBox.Show("서버 연결을 먼저 하세요");
+                return;
+            }
+
+            await connection
+                    .SendMessageAsync(new CS_Start{});
+        }
+    }
+}
