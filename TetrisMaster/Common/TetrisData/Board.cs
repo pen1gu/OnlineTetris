671e6b1eea90a45059927b3c34435c0d35417a11
matt4728 <50314810+matt4728@users.noreply.github.com>
2021-05-16 22:44:27 +0900

    TetrisData 내 Board 클래스 추가

---
+++ TetrisMaster/Common/TetrisData/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.TetrisData
+{
+    
+    public class Board
+    {
+        public PieceType[,] board { get; set; }
+        public readonly int ColumnCnt = 20;
+        public readonly int RowCnt = 10;
+
+        public Board()
+        {
+            board = new PieceType[ColumnCnt, RowCnt];
+        }
+
+    }
+}
