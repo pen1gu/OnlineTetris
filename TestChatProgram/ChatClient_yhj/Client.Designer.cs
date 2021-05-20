f4c7f7351545f7786c71d27133162ae7da676cc5
윤현준 <kkkths@naver.com>
2021-05-20 20:37:50 +0900

    재생성
    일반 클라이언트 재생성 ( 전과 같은 handler 오류나서 테트리스 클라이언트로 넘어감)

---
+++ TestChatProgram/ChatClient_yhj/Client.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ChatClient_yhj
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SaveTilePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(263, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 489);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "연결 해제";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2182, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // SaveTilePanel
+            // 
+            this.SaveTilePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SaveTilePanel.Location = new System.Drawing.Point(50, 21);
+            this.SaveTilePanel.Name = "SaveTilePanel";
+            this.SaveTilePanel.Size = new System.Drawing.Size(148, 141);
+            this.SaveTilePanel.TabIndex = 3;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 513);
+            this.Controls.Add(this.SaveTilePanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel SaveTilePanel;
+    }
+}
---
+++ TestChatProgram/ChatClient_yhj/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient_yhj
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+    }
+}
