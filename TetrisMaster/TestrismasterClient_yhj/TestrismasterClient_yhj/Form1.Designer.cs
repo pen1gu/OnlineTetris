b50e14e43e2f24ee361a78c2748805ab21642df4
hyunjun yun <50539255+hyunjunyun@users.noreply.github.com>
2021-05-22 19:55:13 +0900

    기본 ui 및 기능 구성 (#8)
    * 기본 ui 및 기능 구성
    
    * 프로젝트 참조 수정
    
    Server -> Common

--- TetrisMaster/TestrismasterClient_yhj/TestrismasterClient_yhj/Form1.Designer.cs
+++ TetrisMaster/TestrismasterClient_yhj/TestrismasterClient_yhj/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(12, 461);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(84, 26);
+            this.BtnConnect.TabIndex = 0;
+            this.BtnConnect.Text = "연결";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(258, 461);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(84, 26);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "시작";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 432);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(356, 495);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnConnect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
