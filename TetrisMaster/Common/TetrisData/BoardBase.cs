58b3ac2a1caef5fdb43d40f3775d5a951bc98e55
jkw <jkwchunjae@daum.net>
2021-06-05 20:54:49 +0900

    Update BoardBase.cs

--- TetrisMaster/Common/TetrisData/BoardBase.cs
+++ TetrisMaster/Common/TetrisData/BoardBase.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Common
 {
-    
-    public class BoardBase
+    public abstract class BoardBase
     {
-        public PieceType[,] Board { get; set; }
-        public readonly int ColumnCnt = 20;
-        public readonly int RowCnt = 10;
+        public Cell[,] Board { get; set; }
+        public int ColumnCount { get; private set; }
+        public int RowCount { get; private set; }
 
-        public BoardBase()
-        {
-            Board = new PieceType[ColumnCnt, RowCnt];
-        }
+        public abstract void MoveRight();
+        public abstract void MoveLeft();
+        public abstract void MoveDown();
+        public abstract void MoveUp();
+    }
 
+    public class Cell
+    {
+        public CellType CellType { get; set; }
+        public CellColor Color { get; set; }
+    }
+
+    public enum CellType
+    {
+        Empty,
+        Fill,
+        Active,
+    }
+
+    public enum CellColor
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
     }
 }
