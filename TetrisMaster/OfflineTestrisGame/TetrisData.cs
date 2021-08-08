5b6d8acd4b3040fe8d58637533e99f38802a028e
윤현준 <kkkths@naver.com>
2021-08-08 11:21:59 +0900

    움직임 및 블록 크기 재설정 필

---
+++ TetrisMaster/OfflineTestrisGame/TetrisData.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OfflineTestrisGame
+{
+    class TetrisData
+    {
+        GameRule rule = new GameRule();
+        // 여기서 데이터만 바꿔줌, 데이터만 이라는거 명심.
+
+        public CellType[,] board = new CellType[20, 12]
+        int WIDTH = 12;
+        int HEIGHT = 20;
+        int NewBlockX;
+        int NewBlockY;
+        int CurrentX = 0;
+        int CurrentY = 0;
+        PieceType _pieceType;
+        public TetrisData()
+        {
+            CurrentY = NewBlockY = 0;
+            CurrentX = NewBlockX = WIDTH / 2;
+
+            for (int i = 0; i < HEIGHT; i++)
+            {
+                for (int j = 0; i < WIDTH; i++)
+                {
+                    board[i, j] = CellType.Empty;
+                }
+            }
+        }
+
+        // 필요한 것 지속적으로 테트리스가 내려가야함,
+        // 하나의 블록이 떨어졌을 때 다음 블록이 생성되어야함
+        // 각 블록의 크기 정의
+        // Empty 비어있는, Fill 이전에 이미 차있는, Active 현재 움직이는
+        // 왼쪽 위를 기준으로 프로그래밍.
+        // 움직임만을 담당하는 클래스\
+        // 닿았을 때는 상관 없고 그냥 있는지 확인만 하면 된다.
+
+        private PieceType SetRandomBlock()
+        {
+            Random random = new Random();
+            return (PieceType)random.Next(0, 7);
+        }
+
+        public void MakeNewBlock()
+        {
+            CurrentX = 4;
+            CurrentY = 0;
+            _pieceType = SetRandomBlock();
+            MergeBlockToBoard();
+            // 데이터에 블록 추가
+        }
+
+        public void MergeBlockToBoard()
+        {
+            
+            CellType[,] block = GetBlockObject(_pieceType, Direction.Up);
+            int arrayLength = block.Length;
+            int size = 0;
+
+            switch (arrayLength)
+            {
+                case 4:
+                    size = 2;
+                    break;
+                case 9:
+                    size = 3;
+                    break;
+                case 16:
+                    size = 4;
+                    break;
+
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (block[i, j] == CellType.Active)
+                    {
+                        board[CurrentX + i, CurrentY + j] = 2;
+                    }
+                }
+            }
+        }
+
+        public CellType[,] GetBlockObject(PieceType type, Direction direction)
+        {
+            CellType[,] block = null;
+            switch (type)
+            {
+                case PieceType.A:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                        case Direction.Down:
+                            block = new CellType[4, 4] { { CellType.Empty, CellType.Empty , CellType.Empty , CellType.Empty },{ CellType.Active, CellType.Active, CellType.Active, CellType.Active } };
+                            break;
+                        case Direction.Left:
+                        case Direction.Right:
+                            block = new CellType[4, 4] { { CellType.Active }, { CellType.Active }, { CellType.Active }, { CellType.Active } };
+                            break;
+                    }
+                    break;
+                case PieceType.B:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 3]{
+                                { CellType.Empty, CellType.Active, CellType.Empty},
+                                {CellType.Active, CellType.Active,CellType.Active }
+                            };
+                            break;
+
+                        case Direction.Left:
+                            block = new CellType[3, 2]{
+                                { CellType.Empty, CellType.Active},
+                                {CellType.Active, CellType.Active},
+                                {CellType.Empty, CellType.Active}
+                            };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[3, 2]{
+                                { CellType.Active, CellType.Empty},
+                                {CellType.Active, CellType.Active},
+                                {CellType.Active, CellType.Empty}
+                            };
+                            break;
+                    }
+                    break;
+                case PieceType.C:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 2]
+                    {
+                        { CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active}
+                    };
+                            break;
+
+                        case Direction.Left:
+                            block = new CellType[2, 2]
+              {
+                        { CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active}
+              };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[2, 2]
+              {
+                        { CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active}
+              };
+                            break;
+                    }
+                    break;
+                case PieceType.D:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 3]
+                {
+                        { CellType.Empty, CellType.Empty, CellType.Active},
+                        {CellType.Active, CellType.Active,CellType.Active }
+                };
+                            break;
+
+                        case Direction.Left:
+                            block = new CellType[3, 2]
+                {
+                        { CellType.Active, CellType.Active},
+                        {CellType.Empty, CellType.Active},
+                        {CellType.Empty,CellType.Active }
+                };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[3, 2]
+               {
+                        { CellType.Active, CellType.Empty  },
+                        {CellType.Active, CellType.Empty},
+                        {CellType.Active,CellType.Active }
+               };
+                            break;
+                    }
+
+                    break;
+                case PieceType.E:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 3]
+                            {
+                                { CellType.Active, CellType.Empty, CellType.Empty},
+                                {CellType.Active, CellType.Active,CellType.Active }
+                           };
+                            break;
+                        case Direction.Left:
+                            block = new CellType[3, 2]
+                   {
+                        { CellType.Active, CellType.Empty},
+                        {CellType.Active, CellType.Active},
+                        {CellType.Empty, CellType.Active }
+                   };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[3, 2]
+                   {
+                        { CellType.Active, CellType.Empty},
+                        {CellType.Active, CellType.Active},
+                        {CellType.Empty, CellType.Active }
+                   };
+                            break;
+                    }
+                    break;
+                case PieceType.F:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Empty, CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active,CellType.Empty }
+                    };
+                            break;
+                        case Direction.Down:
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Empty, CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active,CellType.Empty }
+                    };
+                            break;
+                        case Direction.Left:
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Empty, CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active,CellType.Empty }
+                    };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Empty, CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Active,CellType.Empty }
+                    };
+                            break;
+                    }
+
+                    break;
+                case PieceType.G:
+                    switch (direction)
+                    {
+                        case Direction.Up: // normal
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Active, CellType.Active, CellType.Empty},
+                        {CellType.Empty, CellType.Active,CellType.Active }
+                    };
+                            break;
+                        case Direction.Left:
+                            block = new CellType[3, 2]
+                    {
+                        { CellType.Empty  , CellType.Active},
+                        {CellType.Active, CellType.Active},
+                        {CellType.Active, CellType.Empty }
+                    };
+                            break;
+                        case Direction.Right:
+                            block = new CellType[2, 3]
+                    {
+                        { CellType.Active, CellType.Active, CellType.Empty},
+                        {CellType.Empty, CellType.Active,CellType.Active }
+                    };
+                            break;
+                    }
+                    break;
+            }
+
+
+            return block;
+        }
+
+        public void MoveDown()
+        {
+        
+
+
+        }
+
+        public void MoveRight()
+        {
+
+        }
+
+        public void MoveLeft()
+        {
+
+        }
+
+        private void ChangeTorawd()
+        { }
+
+        private void DestroyBlock()
+        {
+
+        }
+
+
+    }
+}
