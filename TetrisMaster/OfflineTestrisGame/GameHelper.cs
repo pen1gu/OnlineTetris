c2afcd7cb6df7ab245510c37215519fefffd3bb7
hyunjun yun <kkkths@naver.com>
2021-10-03 03:39:44 +0900

    헬퍼
    게임을 쉽게 할 수 있게 도움을 주는 기능

---
+++ TetrisMaster/OfflineTestrisGame/GameHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfflineTestrisGame
+{
+    class GameHelper
+    {
+        // 게임을 더 쉽게할 수 있는 여러가지 기능을 넣을 예정
+        // 끄고 키고 할 수 있다
+        // ex) 블록이 어디에 어떤 모양으로 떨어질지 투명화된 블록으로 미리 보여준다.
+    }
+}
